ce2c846cf2a7867fb47bcbb9bf77105abf4dc3a1
arun_forcepoint_laptop <arunjoseph3007@gmail.com>
2025-03-13 12:29:53 +0530

    Moved polynomial helpers to utils

--- src/utils.ts
+++ src/utils.ts
@@ -47,3 +47,33 @@ export const solveQuadEQ = (
   const d = Math.sqrt(disc);
   return [(-b + d) / (2 * a), (-b - d) / (2 * a)];
 };
+
+export const differentiatePolynomial = (poly: number[]) => {
+  const derivative: number[] = [];
+  for (let i = 1; i < poly.length; i++) {
+    derivative.push(poly[i] * i);
+  }
+
+  return derivative;
+};
+
+const fAt = (poly: number[], x: number) =>
+  poly.map((c, i) => c * x ** i).reduce((a, c) => a + c, 0);
+
+export const solvePolynomial = (
+  poly: number[],
+  startPoint = 1,
+  precision = 0.01
+) => {
+  let y = poly.map((a) => a).reverse();
+  let dy = differentiatePolynomial(y);
+
+  let root = startPoint;
+  let fAtRoot = fAt(y, root);
+  while (fAtRoot > precision) {
+    root = root - fAtRoot / fAt(dy, root);
+    fAtRoot = fAt(y, root);
+  }
+
+  return root;
+};
